69304c90d8b3faed4b853d1c245549e768edea08
Md Akbar Hossain <akbarhossain160@gmail.com>
2023-11-01 10:02:54 +0600

    updated

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\VisitorModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class VisitorController extends Controller
 {
 
     public function index()
     {
+        Gate::authorize('index-visitor');
+
         $data = VisitorModel::orderBy('id', 'desc')->take(500)->get();
 
         return view('pages.visitor.index', compact('data'));
